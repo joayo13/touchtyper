0196774e18b7f49be95afb74396efd6af6afb176
Jordan Ayotte <jordanayotte13@outlook.com>
2023-09-26 14:30:39 -0400

    push

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders touch typer h1', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const h1Element = screen.getByText(/touch typer/i);
+  expect(h1Element).toBeInTheDocument();
 });
